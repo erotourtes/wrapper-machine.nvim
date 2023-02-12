0d7b2b637715c85a6a8b1340273267f584080278
Max Sir <erotourtes@gmail.com>
2023-02-12 18:00:18 +0200

    feat: working on both single and multi lines

--- lua/wrapper-machine.lua
+++ lua/wrapper-machine.lua
@@ -1,68 +1,32 @@
+local set_single_line = require("set_single_line")
+local set_multi_lines = require("set_multi_lines")
 local get_pos = require("get_pos")
-local bound_lines = require("bound_lines")
-local wrap_string_with = require("wrap_string_with")
-local M = {}
 
-local close_symbols = {
-    ["("] = ")",
-    ["["] = "]",
-    ["{"] = "}",
-    ["<"] = ">",
-    ['"'] = '"',
-    ["'"] = "'",
-    ["`"] = "`",
-}
+
+local M = {}
 
 local defaults = {
     symbols = { "(", "[", "{", "<", '"', "'", "`" },
+    close_symbols = {
+        ["("] = ")",
+        ["["] = "]",
+        ["{"] = "}",
+        ["<"] = ">",
+        ['"'] = '"',
+        ["'"] = "'",
+        ["`"] = "`",
+    },
     keymap = "<leader>",
 }
 
 local function init(config)
     local function wrap_text_with(symbol)
         local start_pos, end_pos = get_pos()
-        local start_line, end_line = bound_lines(start_pos, end_pos)
-
-        local function set_one_line()
-            local first_sel_ch = start_line:sub(start_pos[2] + 1, start_pos[2] + 1)
-            local last_sel_ch = start_line:sub(end_pos[2] + 1, end_pos[2] + 1)
-
-            for _, key in ipairs(config.symbols) do
-                if (first_sel_ch == key and last_sel_ch == close_symbols[key]) then
-                    start_line = start_line:sub(1, start_pos[2]) .. start_line:sub(start_pos[2] + 2, end_pos[2]) .. start_line:sub(end_pos[2] + 2, #start_line)
-
-                    if (first_sel_ch == symbol) then
-                        vim.api.nvim_buf_set_lines(0, start_pos[1] - 1, start_pos[1], false, { start_line })
-                        return
-                    end
-
-                    break;
-                end
-            end
-
-            local wrapped_line = wrap_string_with(symbol, start_line, start_pos[2])
-            wrapped_line = wrap_string_with(close_symbols[symbol], wrapped_line, end_pos[2] + 2)
-            vim.api.nvim_buf_set_lines(0, start_pos[1] - 1, start_pos[1], false, { wrapped_line })
-        end
-
-        -- local function set_multi_line()
-        --     local wrapped_start = wrap_string_with(symbol, start_line, start_pos[2])
-        --     local wrapped_end = wrap_string_with(close_symbols[symbol], end_line, end_pos[2] + 1)
-        --
-        --     vim.api.nvim_buf_set_lines(
-        --         0,
-        --         start_pos[1] - 1,
-        --         start_pos[1],
-        --         false,
-        --         { wrapped_start }
-        --     )
-        --     vim.api.nvim_buf_set_lines(0, end_pos[1] - 1, end_pos[1], false, { wrapped_end })
-        -- end
 
         if start_pos[1] == end_pos[1] then
-            set_one_line()
+            set_single_line(start_pos, end_pos, symbol, config)
         else
-            -- set_multi_line()
+            set_multi_lines(start_pos, end_pos, symbol, config)
         end
     end
 
